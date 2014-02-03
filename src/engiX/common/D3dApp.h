5761e9fa1bfb774655092176211f43f6baf8c54e
MHesham <mhesham.fcis@gmail.com>
2014-02-03 00:24:30 -0800

    Recreate solution dir structure and create initial sample that links with engiX
    Signed-off-by: MHesham <mhesham.fcis@gmail.com>

---
+++ src/engiX/common/D3dApp.h
@@ -0,0 +1,76 @@
+//***************************************************************************************
+// Modified version of d3dApp.h by Frank Luna (C) 2011.
+//
+// Simple Direct3D demo application class.  
+// Make sure you link: d3d11.lib d3dx11d.lib D3DCompiler.lib D3DX11EffectsD.lib 
+//                     dxerr.lib dxgi.lib dxguid.lib.
+// Link d3dx11.lib and D3DX11Effects.lib for release mode builds instead
+//   of d3dx11d.lib and D3DX11EffectsD.lib.
+//***************************************************************************************
+
+#include "d3dUtil.h"
+#include "Timer.h"
+#include <string>
+
+namespace engiX
+{
+    class D3dApp
+    {
+    public:
+        D3dApp(HINSTANCE hInstance);
+        virtual ~D3dApp();
+
+        HINSTANCE AppInst()const;
+        HWND      MainWnd()const;
+        real     AspectRatio()const;
+
+        int Run();
+
+        // Framework methods.  Derived client class overrides these methods to 
+        // implement specific application requirements.
+
+        virtual bool VInit();
+        virtual void VOnResize(); 
+        virtual void VUpdateScene(const Timer& time)=0;
+        virtual void VDrawScene()=0; 
+        virtual LRESULT VMsgProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam);
+
+        // Convenience overrides for handling mouse input.
+        virtual void VOnMouseDown(WPARAM btnState, int x, int y){ }
+        virtual void VOnMouseUp(WPARAM btnState, int x, int y)  { }
+        virtual void VOnMouseMove(WPARAM btnState, int x, int y){ }
+
+    protected:
+        bool InitMainWindow();
+        bool InitDirect3D();
+
+        void CalculateFrameStats();
+
+    protected:
+
+        HINSTANCE mhAppInst;
+        HWND      mhMainWnd;
+        bool      mAppPaused;
+        bool      mMinimized;
+        bool      mMaximized;
+        bool      mResizing;
+        UINT      m4xMsaaQuality;
+
+        Timer mTimer;
+
+        ID3D11Device* md3dDevice;
+        ID3D11DeviceContext* md3dImmediateContext;
+        IDXGISwapChain* mSwapChain;
+        ID3D11Texture2D* mDepthStencilBuffer;
+        ID3D11RenderTargetView* mRenderTargetView;
+        ID3D11DepthStencilView* mDepthStencilView;
+        D3D11_VIEWPORT mScreenViewport;
+
+        // Derived class should set these in derived constructor to customize starting values.
+        std::wstring mMainWndCaption;
+        D3D_DRIVER_TYPE md3dDriverType;
+        int mClientWidth;
+        int mClientHeight;
+        bool mEnable4xMsaa;
+    };
+}
---
+++ src/engiX/common/Vector.cpp
@@ -0,0 +1,7 @@
+#include "Vector.h"
+
+using namespace engiX;
+
+const static Vector3<real> UP = Vector3<real>(0, 1.0, 0);
+const static Vector3<real> ZERO = Vector3<real>(0, 0, 0);
+const static Vector3<real> ONE = Vector3<real>(1.0, 1.0, 1.0);
