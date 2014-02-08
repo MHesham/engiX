c2a918c8908d33619212cd5b34447c07b1452cd7
MHesham <mhesham.fcis@gmail.com>
2014-02-07 16:49:03 -0800

    Support compiling effects at compile time
    Signed-off-by: MHesham <mhesham.fcis@gmail.com>

--- src/SolarSample/build/main.cpp
+++ src/SolarSample/build/main.cpp
@@ -2,55 +2,58 @@
 #include "D3dApp.h"
 #include "engiX.h"
 #include "Logger.h"
+#include <fstream>
+#include <vector>
 
+using namespace std;
 using namespace engiX;
 
 
 struct Vertex
 {
-	XMFLOAT3 Pos;
-	XMFLOAT4 Color;
+    XMFLOAT3 Pos;
+    XMFLOAT4 Color;
 };
 
 class BoxApp : public D3dApp
 {
 public:
-	BoxApp(HINSTANCE hInstance);
-	~BoxApp();
-	bool VInit();
+    BoxApp(HINSTANCE hInstance);
+    ~BoxApp();
+    bool VInit();
 
 protected:
-	void VOnResize();
-	void VUpdateScene(const Timer& timer);
-	void VDrawScene(); 
+    void VOnResize();
+    void VUpdateScene(const Timer& timer);
+    void VDrawScene(); 
 
-	void VOnMouseDown(WPARAM btnState, int x, int y);
-	void VOnMouseUp(WPARAM btnState, int x, int y);
-	void VOnMouseMove(WPARAM btnState, int x, int y);
+    void VOnMouseDown(WPARAM btnState, int x, int y);
+    void VOnMouseUp(WPARAM btnState, int x, int y);
+    void VOnMouseMove(WPARAM btnState, int x, int y);
 
 private:
-	void BuildGeometryBuffers();
-	void BuildFX();
-	void BuildVertexLayout();
+    void BuildGeometryBuffers();
+    void BuildFX();
+    void BuildVertexLayout();
 
-	ID3D11Buffer* mBoxVB;
-	ID3D11Buffer* mBoxIB;
+    ID3D11Buffer* mBoxVB;
+    ID3D11Buffer* mBoxIB;
 
-	ID3DX11Effect* mFX;
-	ID3DX11EffectTechnique* mTech;
-	ID3DX11EffectMatrixVariable* mfxWorldViewProj;
+    ID3DX11Effect* mFX;
+    ID3DX11EffectTechnique* mTech;
+    ID3DX11EffectMatrixVariable* mfxWorldViewProj;
 
-	ID3D11InputLayout* mInputLayout;
+    ID3D11InputLayout* mInputLayout;
 
-	XMFLOAT4X4 mWorld;
-	XMFLOAT4X4 mView;
-	XMFLOAT4X4 mProj;
+    XMFLOAT4X4 mWorld;
+    XMFLOAT4X4 mView;
+    XMFLOAT4X4 mProj;
 
-	real mTheta;
-	real mPhi;
-	real mRadius;
+    real mTheta;
+    real mPhi;
+    real mRadius;
 
-	POINT mLastMousePos;
+    POINT mLastMousePos;
 };
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE prevInstance,
@@ -83,158 +86,158 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE prevInstance,
 }
 
 BoxApp::BoxApp(HINSTANCE hInstance)
-: D3dApp(hInstance), mBoxVB(0), mBoxIB(0), mFX(0), mTech(0),
-  mfxWorldViewProj(0), mInputLayout(0), 
-  mTheta(1.5f*MathHelper::Pi), mPhi(0.25f*MathHelper::Pi), mRadius(5.0f)
+    : D3dApp(hInstance), mBoxVB(0), mBoxIB(0), mFX(0), mTech(0),
+    mfxWorldViewProj(0), mInputLayout(0), 
+    mTheta(1.5f*MathHelper::Pi), mPhi(0.25f*MathHelper::Pi), mRadius(5.0f)
 {
-	mMainWndCaption = L"Box Demo";
-	
-	mLastMousePos.x = 0;
-	mLastMousePos.y = 0;
+    mMainWndCaption = L"Box Demo";
 
-	XMMATRIX I = XMMatrixIdentity();
-	XMStoreFloat4x4(&mWorld, I);
-	XMStoreFloat4x4(&mView, I);
-	XMStoreFloat4x4(&mProj, I);
+    mLastMousePos.x = 0;
+    mLastMousePos.y = 0;
+
+    XMMATRIX I = XMMatrixIdentity();
+    XMStoreFloat4x4(&mWorld, I);
+    XMStoreFloat4x4(&mView, I);
+    XMStoreFloat4x4(&mProj, I);
 }
 
 BoxApp::~BoxApp()
 {
-	SafeRelease(mBoxVB);
-	SafeRelease(mBoxIB);
-	SafeRelease(mFX);
-	SafeRelease(mInputLayout);
+    SafeRelease(mBoxVB);
+    SafeRelease(mBoxIB);
+    SafeRelease(mFX);
+    SafeRelease(mInputLayout);
 }
 
 bool BoxApp::VInit()
 {
-	if(!D3dApp::VInit())
-		return false;
+    if(!D3dApp::VInit())
+        return false;
 
-	BuildGeometryBuffers();
-	BuildFX();
-	BuildVertexLayout();
+    BuildGeometryBuffers();
+    BuildFX();
+    BuildVertexLayout();
 
-	return true;
+    return true;
 }
 
 void BoxApp::VOnResize()
 {
-	D3dApp::VOnResize();
+    D3dApp::VOnResize();
 
-	// The window resized, so update the aspect ratio and recompute the projection matrix.
-	XMMATRIX P = XMMatrixPerspectiveFovLH(0.25f*MathHelper::Pi, AspectRatio(), 1.0f, 1000.0f);
-	XMStoreFloat4x4(&mProj, P);
+    // The window resized, so update the aspect ratio and recompute the projection matrix.
+    XMMATRIX P = XMMatrixPerspectiveFovLH(0.25f*MathHelper::Pi, AspectRatio(), 1.0f, 1000.0f);
+    XMStoreFloat4x4(&mProj, P);
 }
 
 void BoxApp::VUpdateScene(const Timer& timer)
 {
-	// Convert Spherical to Cartesian coordinates.
-	real x = mRadius*sinf(mPhi)*cosf(mTheta);
-	real z = mRadius*sinf(mPhi)*sinf(mTheta);
-	real y = mRadius*cosf(mPhi);
+    // Convert Spherical to Cartesian coordinates.
+    real x = mRadius*sinf(mPhi)*cosf(mTheta);
+    real z = mRadius*sinf(mPhi)*sinf(mTheta);
+    real y = mRadius*cosf(mPhi);
 
-	// Build the view matrix.
-	XMVECTOR pos    = XMVectorSet(x, y, z, 1.0f);
-	XMVECTOR target = XMVectorZero();
-	XMVECTOR up     = XMVectorSet(0.0f, 1.0f, 0.0f, 0.0f);
+    // Build the view matrix.
+    XMVECTOR pos    = XMVectorSet(x, y, z, 1.0f);
+    XMVECTOR target = XMVectorZero();
+    XMVECTOR up     = XMVectorSet(0.0f, 1.0f, 0.0f, 0.0f);
 
-	XMMATRIX V = XMMatrixLookAtLH(pos, target, up);
-	XMStoreFloat4x4(&mView, V);
+    XMMATRIX V = XMMatrixLookAtLH(pos, target, up);
+    XMStoreFloat4x4(&mView, V);
 }
 
 void BoxApp::VDrawScene()
 {
-	md3dImmediateContext->ClearRenderTargetView(mRenderTargetView, reinterpret_cast<const real*>(&Colors::LightSteelBlue));
-	md3dImmediateContext->ClearDepthStencilView(mDepthStencilView, D3D11_CLEAR_DEPTH|D3D11_CLEAR_STENCIL, 1.0f, 0);
+    md3dImmediateContext->ClearRenderTargetView(mRenderTargetView, reinterpret_cast<const real*>(&Colors::LightSteelBlue));
+    md3dImmediateContext->ClearDepthStencilView(mDepthStencilView, D3D11_CLEAR_DEPTH|D3D11_CLEAR_STENCIL, 1.0f, 0);
 
-	md3dImmediateContext->IASetInputLayout(mInputLayout);
+    md3dImmediateContext->IASetInputLayout(mInputLayout);
     md3dImmediateContext->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
 
-	UINT stride = sizeof(Vertex);
+    UINT stride = sizeof(Vertex);
     UINT offset = 0;
     md3dImmediateContext->IASetVertexBuffers(0, 1, &mBoxVB, &stride, &offset);
-	md3dImmediateContext->IASetIndexBuffer(mBoxIB, DXGI_FORMAT_R32_UINT, 0);
+    md3dImmediateContext->IASetIndexBuffer(mBoxIB, DXGI_FORMAT_R32_UINT, 0);
 
-	// Set constants
-	XMMATRIX world = XMLoadFloat4x4(&mWorld);
-	XMMATRIX view  = XMLoadFloat4x4(&mView);
-	XMMATRIX proj  = XMLoadFloat4x4(&mProj);
-	XMMATRIX worldViewProj = world*view*proj;
+    // Set constants
+    XMMATRIX world = XMLoadFloat4x4(&mWorld);
+    XMMATRIX view  = XMLoadFloat4x4(&mView);
+    XMMATRIX proj  = XMLoadFloat4x4(&mProj);
+    XMMATRIX worldViewProj = world*view*proj;
 
-	mfxWorldViewProj->SetMatrix(reinterpret_cast<real*>(&worldViewProj));
+    mfxWorldViewProj->SetMatrix(reinterpret_cast<real*>(&worldViewProj));
 
     D3DX11_TECHNIQUE_DESC techDesc;
     mTech->GetDesc( &techDesc );
     for(UINT p = 0; p < techDesc.Passes; ++p)
     {
         mTech->GetPassByIndex(p)->Apply(0, md3dImmediateContext);
-        
-		// 36 indices for the box.
-		md3dImmediateContext->DrawIndexed(36, 0, 0);
+
+        // 36 indices for the box.
+        md3dImmediateContext->DrawIndexed(36, 0, 0);
     }
 
-	HR(mSwapChain->Present(0, 0));
+    HR(mSwapChain->Present(0, 0));
 }
 
 void BoxApp::VOnMouseDown(WPARAM btnState, int x, int y)
 {
-	mLastMousePos.x = x;
-	mLastMousePos.y = y;
+    mLastMousePos.x = x;
+    mLastMousePos.y = y;
 
-	SetCapture(mhMainWnd);
+    SetCapture(mhMainWnd);
 }
 
 void BoxApp::VOnMouseUp(WPARAM btnState, int x, int y)
 {
-	ReleaseCapture();
+    ReleaseCapture();
 }
 
 void BoxApp::VOnMouseMove(WPARAM btnState, int x, int y)
 {
-	if( (btnState & MK_LBUTTON) != 0 )
-	{
-		// Make each pixel correspond to a quarter of a degree.
-		real dx = XMConvertToRadians(0.25f*static_cast<real>(x - mLastMousePos.x));
-		real dy = XMConvertToRadians(0.25f*static_cast<real>(y - mLastMousePos.y));
+    if( (btnState & MK_LBUTTON) != 0 )
+    {
+        // Make each pixel correspond to a quarter of a degree.
+        real dx = XMConvertToRadians(0.25f*static_cast<real>(x - mLastMousePos.x));
+        real dy = XMConvertToRadians(0.25f*static_cast<real>(y - mLastMousePos.y));
 
-		// Update angles based on input to orbit camera around box.
-		mTheta += dx;
-		mPhi   += dy;
+        // Update angles based on input to orbit camera around box.
+        mTheta += dx;
+        mPhi   += dy;
 
-		// Restrict the angle mPhi.
-		mPhi = MathHelper::Clamp(mPhi, 0.1f, MathHelper::Pi-0.1f);
-	}
-	else if( (btnState & MK_RBUTTON) != 0 )
-	{
-		// Make each pixel correspond to 0.005 unit in the scene.
-		real dx = 0.005f*static_cast<real>(x - mLastMousePos.x);
-		real dy = 0.005f*static_cast<real>(y - mLastMousePos.y);
+        // Restrict the angle mPhi.
+        mPhi = MathHelper::Clamp(mPhi, 0.1f, MathHelper::Pi-0.1f);
+    }
+    else if( (btnState & MK_RBUTTON) != 0 )
+    {
+        // Make each pixel correspond to 0.005 unit in the scene.
+        real dx = 0.005f*static_cast<real>(x - mLastMousePos.x);
+        real dy = 0.005f*static_cast<real>(y - mLastMousePos.y);
 
-		// Update the camera radius based on input.
-		mRadius += dx - dy;
+        // Update the camera radius based on input.
+        mRadius += dx - dy;
 
-		// Restrict the radius.
-		mRadius = MathHelper::Clamp(mRadius, 3.0f, 15.0f);
-	}
+        // Restrict the radius.
+        mRadius = MathHelper::Clamp(mRadius, 3.0f, 15.0f);
+    }
 
-	mLastMousePos.x = x;
-	mLastMousePos.y = y;
+    mLastMousePos.x = x;
+    mLastMousePos.y = y;
 }
 
 void BoxApp::BuildGeometryBuffers()
 {
-	// Create vertex buffer
+    // Create vertex buffer
     Vertex vertices[] =
     {
-		{ XMFLOAT3(-1.0f, -1.0f, -1.0f), (const real*)&Colors::White   },
-		{ XMFLOAT3(-1.0f, +1.0f, -1.0f), (const real*)&Colors::Black   },
-		{ XMFLOAT3(+1.0f, +1.0f, -1.0f), (const real*)&Colors::Red     },
-		{ XMFLOAT3(+1.0f, -1.0f, -1.0f), (const real*)&Colors::Green   },
-		{ XMFLOAT3(-1.0f, -1.0f, +1.0f), (const real*)&Colors::Blue    },
-		{ XMFLOAT3(-1.0f, +1.0f, +1.0f), (const real*)&Colors::Yellow  },
-		{ XMFLOAT3(+1.0f, +1.0f, +1.0f), (const real*)&Colors::Cyan    },
-		{ XMFLOAT3(+1.0f, -1.0f, +1.0f), (const real*)&Colors::Magenta }
+        { XMFLOAT3(-1.0f, -1.0f, -1.0f), (const real*)&Colors::White   },
+        { XMFLOAT3(-1.0f, +1.0f, -1.0f), (const real*)&Colors::Black   },
+        { XMFLOAT3(+1.0f, +1.0f, -1.0f), (const real*)&Colors::Red     },
+        { XMFLOAT3(+1.0f, -1.0f, -1.0f), (const real*)&Colors::Green   },
+        { XMFLOAT3(-1.0f, -1.0f, +1.0f), (const real*)&Colors::Blue    },
+        { XMFLOAT3(-1.0f, +1.0f, +1.0f), (const real*)&Colors::Yellow  },
+        { XMFLOAT3(+1.0f, +1.0f, +1.0f), (const real*)&Colors::Cyan    },
+        { XMFLOAT3(+1.0f, -1.0f, +1.0f), (const real*)&Colors::Magenta }
     };
 
     D3D11_BUFFER_DESC vbd;
@@ -243,100 +246,92 @@ void BoxApp::BuildGeometryBuffers()
     vbd.BindFlags = D3D11_BIND_VERTEX_BUFFER;
     vbd.CPUAccessFlags = 0;
     vbd.MiscFlags = 0;
-	vbd.StructureByteStride = 0;
+    vbd.StructureByteStride = 0;
     D3D11_SUBRESOURCE_DATA vinitData;
     vinitData.pSysMem = vertices;
     HR(md3dDevice->CreateBuffer(&vbd, &vinitData, &mBoxVB));
 
 
-	// Create the index buffer
+    // Create the index buffer
 
-	UINT indices[] = {
-		// front face
-		0, 1, 2,
-		0, 2, 3,
+    UINT indices[] = {
+        // front face
+        0, 1, 2,
+        0, 2, 3,
 
-		// back face
-		4, 6, 5,
-		4, 7, 6,
+        // back face
+        4, 6, 5,
+        4, 7, 6,
 
-		// left face
-		4, 5, 1,
-		4, 1, 0,
+        // left face
+        4, 5, 1,
+        4, 1, 0,
 
-		// right face
-		3, 2, 6,
-		3, 6, 7,
+        // right face
+        3, 2, 6,
+        3, 6, 7,
 
-		// top face
-		1, 5, 6,
-		1, 6, 2,
+        // top face
+        1, 5, 6,
+        1, 6, 2,
 
-		// bottom face
-		4, 0, 3, 
-		4, 3, 7
-	};
+        // bottom face
+        4, 0, 3, 
+        4, 3, 7
+    };
 
-	D3D11_BUFFER_DESC ibd;
+    D3D11_BUFFER_DESC ibd;
     ibd.Usage = D3D11_USAGE_IMMUTABLE;
     ibd.ByteWidth = sizeof(UINT) * 36;
     ibd.BindFlags = D3D11_BIND_INDEX_BUFFER;
     ibd.CPUAccessFlags = 0;
     ibd.MiscFlags = 0;
-	ibd.StructureByteStride = 0;
+    ibd.StructureByteStride = 0;
     D3D11_SUBRESOURCE_DATA iinitData;
     iinitData.pSysMem = indices;
     HR(md3dDevice->CreateBuffer(&ibd, &iinitData, &mBoxIB));
 }
- 
+
 void BoxApp::BuildFX()
 {
-	DWORD shaderFlags = 0;
-#if defined( DEBUG ) || defined( _DEBUG )
-    shaderFlags |= D3D10_SHADER_DEBUG;
-	shaderFlags |= D3D10_SHADER_SKIP_OPTIMIZATION;
-#endif
- 
-	ID3D10Blob* compiledShader = 0;
-	ID3D10Blob* compilationMsgs = 0;
-	HRESULT hr = D3DX11CompileFromFile(L"FX/color.fx", 0, 0, 0, "fx_5_0", shaderFlags, 
-		0, 0, &compiledShader, &compilationMsgs, 0);
+    std::string fxFilename = "data\\fx\\color.fxo";
+    ifstream fxFile;
 
-	// compilationMsgs can store errors or warnings.
-	if( compilationMsgs != 0 )
-	{
-		MessageBoxA(0, (char*)compilationMsgs->GetBufferPointer(), 0, 0);
-		SafeRelease(compilationMsgs);
-	}
+    fxFile.open(fxFilename.c_str(), ios::in | ios::binary);
 
-	// Even if there are no compilationMsgs, check to make sure there were no other errors.
-	if(FAILED(hr))
-	{
-		DXTrace(__FILE__, (DWORD)__LINE__, hr, L"D3DX11CompileFromFile", true);
-	}
+    if (!fxFile.is_open())
+    {
+        LogError("Failed to open fx file %s", fxFilename.c_str());
+        return;
+    }
 
-	HR(D3DX11CreateEffectFromMemory(compiledShader->GetBufferPointer(), compiledShader->GetBufferSize(), 
-		0, md3dDevice, &mFX));
+    fxFile.seekg(0, ios::end);
+    size_t fxBinarySize = (size_t)fxFile.tellg();
+    fxFile.seekg(0, ios::beg);
 
-	// Done with compiled shader.
-	SafeRelease(compiledShader);
+    vector<char> fxBinary(fxBinarySize);
+    fxFile.read(&fxBinary[0], fxBinarySize);
+    fxFile.close();
 
-	mTech    = mFX->GetTechniqueByName("ColorTech");
-	mfxWorldViewProj = mFX->GetVariableByName("gWorldViewProj")->AsMatrix();
+    HR(D3DX11CreateEffectFromMemory((const void*)&fxBinary[0], (SIZE_T)fxBinarySize, 
+        0, md3dDevice, &mFX));
+
+    mTech    = mFX->GetTechniqueByName("ColorTech");
+    mfxWorldViewProj = mFX->GetVariableByName("gWorldViewProj")->AsMatrix();
 }
 
 void BoxApp::BuildVertexLayout()
 {
-	// Create the vertex input layout.
-	D3D11_INPUT_ELEMENT_DESC vertexDesc[] =
-	{
-		{"POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 0, D3D11_INPUT_PER_VERTEX_DATA, 0},
-		{"COLOR",    0, DXGI_FORMAT_R32G32B32A32_FLOAT, 0, 12, D3D11_INPUT_PER_VERTEX_DATA, 0}
-	};
+    // Create the vertex input layout.
+    D3D11_INPUT_ELEMENT_DESC vertexDesc[] =
+    {
+        {"POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 0, D3D11_INPUT_PER_VERTEX_DATA, 0},
+        {"COLOR",    0, DXGI_FORMAT_R32G32B32A32_FLOAT, 0, 12, D3D11_INPUT_PER_VERTEX_DATA, 0}
+    };
 
-	// Create the input layout
+    // Create the input layout
     D3DX11_PASS_DESC passDesc;
     mTech->GetPassByIndex(0)->GetDesc(&passDesc);
-	HR(md3dDevice->CreateInputLayout(vertexDesc, 2, passDesc.pIAInputSignature, 
-		passDesc.IAInputSignatureSize, &mInputLayout));
+    HR(md3dDevice->CreateInputLayout(vertexDesc, 2, passDesc.pIAInputSignature, 
+        passDesc.IAInputSignatureSize, &mInputLayout));
 }
